cb52fa8822b8ac5fcde208f1983e50a1d0181422
Sergio López <sergio.lopez@holasoft.es>
2023-08-23 12:05:07 +0200

    Mejoras en archivos, creación de variables.yml y hola_github.py

--- src/hola_empresa.py
+++ src/hola_empresa.py
@@ -1,10 +1,8 @@
 import os
 
-
 def main():
     nombre = os.getenv("USERNAME")
     print(f"Mi empresa se llama {nombre} y esto es un curso que me ha proporcionado.")
-
 
 if __name__ == "__main__":
     main()
---
+++ src/hola_github.py
@@ -0,0 +1,12 @@
+import os
+
+def main():
+    nombre = os.getenv("USERNAME")
+    edad = os.getenv("AGE")
+    localidad = os.getenv("LOCATION")
+    pais = os.getenv("COUNTRY")
+    informacion = os.getenv("INFORMATION")
+    print(f"¡Hola, {nombre} tengo {edad}. Actualmente resido en {localidad} que se encuentra en {pais}. Gracias a seguir estudiando puedo decir que {informacion}!")
+
+if __name__ == "__main__":
+    main()
